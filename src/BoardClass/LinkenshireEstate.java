5630fb9d789725769be3d3d751a56c3b5692496c
BT_1N3_31 <baishakicodes>
2018-12-05 08:43:01 -0500

    need to fix board class

--- src/BoardClass/LinkenshireEstate.java
+++ src/BoardClass/LinkenshireEstate.java
@@ -1,25 +1,24 @@
 package BoardClass;
 
 import java.util.Scanner;
-
+import Rooms.Room;
 
 public class LinkenshireEstate {
-    private String[][] Estate;
-    public LinkenshireEstate(String[][] Estate){
-        this.Estate=Estate;
+    private Room[][] Estate;
+    private int width;
+    public LinkenshireEstate(Room[][] Estate){
+        this.Estate= Estate;
     }
-    public void arrayConfig(int numRows, int numCols){
-        Scanner arrayConfig = new Scanner(System.in);
-        System.out.println("How big would you like the estate to be? Choose 4(for 4 by 4) or 5(for 5 by 5).");
-        int config = arrayConfig.nextInt();
-        if(config==4){
-            Estate = new String[4][4];
-        }
-        if(config==5){
-            Estate = new String[5][5];
-        }
-        else{
-            System.out.println("That's not a valid input. Choose either 4 or 5!");
+    public LinkenshireEstate(int width){
+        this.width = width;
+        Estate = new Room[width][width];
+    }
+    public void printEstate(){
+        for(Room[] i : Estate){
+            for(Room j : i){
+                System.out.print(j);
+            }
+            System.out.println();
         }
     }
 }
